8b7167f183743c257359ce9591878295ff6a9d80
manhualu <manhua.lu64@gmail.com>
2022-12-14 02:27:43 +1100

    Diameter of Binary Tree

---
+++ DiameterOfBinaryTree.py
@@ -0,0 +1,34 @@
+"""
+Diameter of Binary Tree
+    EASY (Wed 14 Dec 2022)
+"""
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+
+        if root is None:
+            return 0
+        
+        leftHeight = self.getHeight(root.left)
+        rightHeight = self.getHeight(root.right)
+
+        leftDiameter = self.diameterOfBinaryTree(root.left)
+        rightDiameter = self.diameterOfBinaryTree(root.right)
+
+        return max(leftHeight + rightHeight, max(leftDiameter, rightDiameter))
+
+
+    def getHeight(self, root: Optional[TreeNode]) -> int:
+        if root is None:
+            return 0
+
+        leftHeight = self.getHeight(root.left)
+        rightHeight = self.getHeight(root.right)
+
+        return max(leftHeight, rightHeight) + 1
